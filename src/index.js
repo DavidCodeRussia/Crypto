4bba5b09047b2c38deebd9feb36aba121e9bcf3b
David <david_arakelyan_code@mail.ru>
2021-12-14 03:04:54 +0400

    added funny night

--- src/index.js
+++ src/index.js
@@ -7,19 +7,12 @@ import {BrowserRouter} from "react-router-dom";
 import App from "./App";
 import {Provider} from "react-redux";
 
-let rerenderEntireThree = () => {
     ReactDOM.render(
         <BrowserRouter>
             <Provider store={store} >
                 <App />
             </Provider>
         </BrowserRouter>, document.getElementById('root'))
-}
-
-rerenderEntireThree()
-store.subscribe( () => {
-    rerenderEntireThree()
-} )
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
