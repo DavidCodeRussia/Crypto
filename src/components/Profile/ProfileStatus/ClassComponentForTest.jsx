e600aecbadf49b1660750b72a6390bbe6d8aebd2
David <david_arakelyan_code@mail.ru>
2022-02-09 12:03:15 +0400

    92 success

---
+++ src/components/Profile/ProfileStatus/ClassComponentForTest.jsx
@@ -0,0 +1,55 @@
+import React, {Component} from 'react'
+
+class ClassComponentForTest extends Component {
+
+    state = {
+        editMode: false,
+        status: this.props.status
+    }
+
+    activateEditMode = () => {
+        this.setState({
+            editMode: true
+        })
+    }
+
+    deactivateEditMode = () => {
+        this.setState({
+            editMode: false
+        })
+        this.props.updateStatus(this.props.status)
+    }
+
+    onStatusChange = (e) => {
+        this.setState({
+            status: e.currentTarget.value
+        })
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if(prevProps.status !== this.props.status) {
+            this.setState({
+                status: this.props.status
+            })
+        }
+    }
+
+    render() {
+    return (
+        <div>
+            {!this.state.editMode &&
+            <div>
+                <span onClick={this.activateEditMode}>{this.props.status || "empty status"}</span>
+            </div>
+            }
+            {this.state.editMode &&
+            <div>
+                <input onChange={this.onStatusChange} value={this.status} onBlur={this.deactivateEditMode} autoFocus={true}/>
+            </div>
+            }
+        </div>
+    )
+    }
+}
+
+export default ClassComponentForTest;
