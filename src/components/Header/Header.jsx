7861571f29e72ec1f70510b9ee2bdcf94d7bef03
David <david_arakelyan_code@mail.ru>
2022-04-01 17:28:55 +0400

    workingSocial01.04.22

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,14 @@
 import s from './Header.module.css';
 
 const Header = (props) => {
+
   return (
   <header className={s.header}>
     <img className={s.firstSrc} src='https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Bitcoin.svg/1024px-Bitcoin.svg.png' />
     <div className={s.loginBlock}>
       {props.isAuth
-          && <div className={s.loginName}> {props.login}  <button className={s.logoutButton} onClick={props.logout}>Log out</button></div>
+            && <div className={s.loginName}> {props.login} <button className={s.logoutButton} onClick={props.logout}>Log out</button>
+               </div>
           }
     </div>
   </header>
--- src/components/redux/app-reducer.js
+++ src/components/redux/app-reducer.js
@@ -23,6 +23,8 @@ export const initializedSuccess = () => ({type: INITIALIZED_SUCCESS})
 
 export const initializeApp = () => (dispatch) => {
     let promise = dispatch(getAuthUserData())
+    //dispatch(someThingElse()) к примеру у нас там ещё 2 диспатча. А так можно было бы просто записать promise.then(...)
+    //dispatch(someThingElse()) что так что так можно
     Promise.all([promise])
         .then( () => {
         dispatch(initializedSuccess())
