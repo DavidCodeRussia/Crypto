9efd959a1727623c1188192a6267394076016b32
DavidCodeRussia <93557790+DavidCodeRussia@users.noreply.github.com>
2023-02-09 22:48:11 +0400

    refactor issues

--- src/hoc/withAuthRedirect.ts
+++ src/hoc/withAuthRedirect.ts
@@ -1,24 +1,25 @@
-import React from "react";
-import { Navigate } from "react-router-dom";
-import { connect } from "react-redux";
-import { AppStateType } from "../redux/redux-store";
+import React from 'react';
+import { redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { AppStateType } from '../redux/redux-store';
 
 type TMapState = {
-  
-}
+  isAuth: boolean;
+};
 
 let mapStateToProps = (state: AppStateType) => ({
   isAuth: state.auth.isAuth,
 });
 
 export function withAuthNavigate<WCP>(Component: React.ComponentType<WCP>) {
-  function NavigateComponent(props: WCP) => {    
-      if (!props.isAuth) return <Navigate replace to="/login" />
-      
-      return <Component {...this.props} />;    
+  function NavigateComponent(props: WCP & TMapState) {
+    const { isAuth, ...restProps } = props;
+    if (!props.isAuth) return redirect('/login');
+
+    return <Component {...(restProps as WCP)} />;
   }
 
   let ConnectedAuthNavigateComponent = connect(mapStateToProps)(NavigateComponent);
 
   return ConnectedAuthNavigateComponent;
-};
+}
