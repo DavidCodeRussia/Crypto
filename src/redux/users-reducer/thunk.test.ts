42be045363abb8fe565afdbc62423abff9cadd3e
DavidCodeRussia <93557790+DavidCodeRussia@users.noreply.github.com>
2023-02-16 23:44:38 +0400

    work on tests

--- src/redux/users-reducer/thunk.test.ts
+++ src/redux/users-reducer/thunk.test.ts
@@ -1,26 +1,41 @@
-import { follow, actions } from ".";
-import { APIResponseType, EResultCode } from "../../API/api";
-import { usersAPI } from "../../API/users-api";
+import { follow, actions, unfollow } from '.';
+import { APIResponseType, EResultCode } from '../../API/api';
+import { usersAPI } from '../../API/users-api';
 
-jest.mock("../../API/users-api");
+jest.mock('../../API/users-api');
 
-const usersAPIMock = usersAPI;
+const usersAPIMock = usersAPI as jest.Mocked<typeof usersAPI>;
 const result: APIResponseType = {
   resultCode: EResultCode.Success,
   messages: [],
   data: {},
 };
-// @ts-ignore
-usersAPIMock.follow.mockReturnValue(Promise.resolve(result));
 
-test("follow logic", async () => {
+usersAPIMock.follow.mockReturnValue(Promise.resolve(result));
+usersAPIMock.unfollow.mockReturnValue(Promise.resolve(result));
+
+test('follow logic', async () => {
   const thunk = follow(1);
   const dispatchMock = jest.fn();
-  // @ts-ignore
-  await thunk(dispatchMock);
+  const getStateMock = jest.fn();
+
+  await thunk(dispatchMock, getStateMock, {});
 
   expect(dispatchMock).toBeCalledTimes(3);
-  expect(dispatchMock).toHaveBeenNthCalledWith(1, actions.toggleFollowingProgress(true, 3));
+  expect(dispatchMock).toHaveBeenNthCalledWith(1, actions.toggleFollowingProgress(true, 1));
   expect(dispatchMock).toHaveBeenNthCalledWith(2, actions.followSuccess(3));
-  expect(dispatchMock).toHaveBeenNthCalledWith(2, actions.toggleFollowingProgress(false, 3));
+  expect(dispatchMock).toHaveBeenNthCalledWith(3, actions.toggleFollowingProgress(false, 1));
+});
+
+test('unfollow logic', async () => {
+  const thunk = unfollow(1);
+  const dispatchMock = jest.fn();
+  const getStateMock = jest.fn();
+
+  await thunk(dispatchMock, getStateMock, {});
+
+  expect(dispatchMock).toBeCalledTimes(3);
+  expect(dispatchMock).toHaveBeenNthCalledWith(1, actions.toggleFollowingProgress(true, 1));
+  expect(dispatchMock).toHaveBeenNthCalledWith(2, actions.followSuccess(3));
+  expect(dispatchMock).toHaveBeenNthCalledWith(3, actions.toggleFollowingProgress(false, 1));
 });
