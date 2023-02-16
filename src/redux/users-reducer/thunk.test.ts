c1f0832b28e73c591a5b7ce71527efe5b69eb71b
DavidCodeRussia <david_arakelyan_code@mail.ru>
2023-02-16 11:03:19 +0400

    tests

--- src/redux/users-reducer/thunk.test.ts
+++ src/redux/users-reducer/thunk.test.ts
@@ -1,4 +1,4 @@
-import { follow } from ".";
+import { follow, actions } from ".";
 import { APIResponseType, EResultCode } from "../../API/api";
 import { usersAPI } from "../../API/users-api";
 
@@ -13,11 +13,14 @@ const result: APIResponseType = {
 // @ts-ignore
 usersAPIMock.follow.mockReturnValue(Promise.resolve(result));
 
-test("", () => {
+test("follow logic", async () => {
   const thunk = follow(1);
   const dispatchMock = jest.fn();
   // @ts-ignore
   await thunk(dispatchMock);
 
   expect(dispatchMock).toBeCalledTimes(3);
+  expect(dispatchMock).toHaveBeenNthCalledWith(1, actions.toggleFollowingProgress(true, 3));
+  expect(dispatchMock).toHaveBeenNthCalledWith(2, actions.followSuccess(3));
+  expect(dispatchMock).toHaveBeenNthCalledWith(2, actions.toggleFollowingProgress(false, 3));
 });
