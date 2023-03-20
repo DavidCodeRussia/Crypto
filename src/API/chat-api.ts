5245d9d698c467c80f555c377a1f76f7d929aba1
DavidCodeRussia <93557790+DavidCodeRussia@users.noreply.github.com>
2023-03-20 20:57:30 +0400

    work from home

--- src/API/chat-api.ts
+++ src/API/chat-api.ts
@@ -4,37 +4,37 @@ export type TChatMessage = {
   userId: number;
   userName: string;
 };
-export type TEventsNames = "messages-received" | "status-changed";
-export type TStatus = "pending" | "ready";
+export type TEventsNames = 'messages-received' | 'status-changed';
+export type TStatus = 'pending' | 'ready';
 
 export type TMessagesReceivedSubscriber = (messages: TChatMessage[]) => void;
 export type TStatusChangesSubscriber = (status: TStatus) => void;
-let subscribers = {
-  "messages-received": [] as TMessagesReceivedSubscriber[],
-  "status-changed": [] as TMessagesReceivedSubscriber[],
+const subscribers = {
+  'messages-received': [] as TMessagesReceivedSubscriber[],
+  'status-changed': [] as TMessagesReceivedSubscriber[],
 };
 
 let ws: WebSocket | null = null;
 
 const closeHandler = () => {
-  console.log("WS closed");
+  console.log('WS closed');
   setTimeout(createChannel, 3000);
 };
 const onMessageHandler = (e: MessageEvent) => {
   let newMessage = JSON.parse(e.data);
-  subscribers.forEach((item) => item(newMessage));
+  subscribers['messages-received'].forEach((item) => item(newMessage));
 };
 const cleanUp = () => {
-  ws?.removeEventListener("close", closeHandler);
-  ws?.removeEventListener("message", onMessageHandler);
+  ws?.removeEventListener('close', closeHandler);
+  ws?.removeEventListener('message', onMessageHandler);
 };
 
 const createChannel = () => {
   cleanUp();
   ws?.close();
-  ws = new WebSocket("wss://social-network.samuraijs.com/handlers/ChatHandler.ashx");
-  ws.addEventListener("close", closeHandler);
-  ws.addEventListener("message", onMessageHandler);
+  ws = new WebSocket('wss://social-network.samuraijs.com/handlers/ChatHandler.ashx');
+  ws.addEventListener('close', closeHandler);
+  ws.addEventListener('message', onMessageHandler);
 };
 
 export const chatAPI = {
@@ -42,19 +42,25 @@ export const chatAPI = {
     createChannel();
   },
   stop() {
-    subscribers = [];
+    subscribers['messages-received'] = [];
+    subscribers['status-changed'] = [];
     cleanUp();
     ws?.close();
   },
-  subscribe(eventName: TEventsNames, callback: TMessagesReceivedSubscriber) {
-    subscribers.push(callback);
+  subscribe(
+    eventName: TEventsNames,
+    callback: TMessagesReceivedSubscriber | TStatusChangesSubscriber,
+  ) {
+    // @ts-ignore
+    subscribers[eventName].push(callback);
 
     return () => {
-      subscribers = subscribers.filter((s) => s !== callback);
+      subscribers[eventName] = subscribers[eventName].filter((s) => s !== callback);
     };
   },
-  unsubscribe(callback: TMessagesReceivedSubscriber) {
-    subscribers = subscribers.filter((s) => s !== callback);
+  unsubscribe(eventName: TEventsNames, callback: TMessagesReceivedSubscriber) {
+    // @ts-ignore
+    subscribers[eventName] = subscribers[eventName].filter((s) => s !== callback);
   },
   sendMessage(message: string) {
     ws?.send(message);
--- src/redux/chat-reducer/index.ts
+++ src/redux/chat-reducer/index.ts
@@ -1,24 +1,24 @@
-import { Dispatch } from "redux";
-import { chatAPI, TChatMessage } from "../../API/chat-api";
-import { ThunkType } from "../users-reducer/types";
-import { TActions, TInitialState } from "./types";
-import { TStatus } from "../../API/chat-api";
+import { Dispatch } from 'redux';
+import { chatAPI, TChatMessage } from '../../API/chat-api';
+import { ThunkType } from '../users-reducer/types';
+import { TActions, TInitialState } from './types';
+import { TStatus } from '../../API/chat-api';
 
-export const ADD_MESSAGE = "add";
+export const ADD_MESSAGE = 'add';
 
 export let initialState = {
   messages: [] as TChatMessage[],
-  status: "pending" as TStatus,
+  status: 'pending' as TStatus,
 };
 
 const chatReducer = (state = initialState, action: TActions): TInitialState => {
   switch (action.type) {
-    case "chat-reducer/MESSAGES_RECEIVED":
+    case 'chat-reducer/MESSAGES_RECEIVED':
       return {
         ...state,
         messages: [...state.messages, ...action.payload.messages],
       };
-    case "chat-reducer/STATUS_CHANGED":
+    case 'chat-reducer/STATUS_CHANGED':
       return {
         ...state,
         status: action.payload.status,
@@ -30,11 +30,11 @@ const chatReducer = (state = initialState, action: TActions): TInitialState => {
 
 export const actions = {
   messagesReceived: (messages: TChatMessage[]) => ({
-    type: "chat-reducer/MESSAGES_RECEIVED",
+    type: 'chat-reducer/MESSAGES_RECEIVED',
     payload: { messages },
   }),
   statusChanged: (status: TStatus) => ({
-    type: "chat-reducer/STATUS_CHANGED",
+    type: 'chat-reducer/STATUS_CHANGED',
     payload: { status },
   }),
 };
@@ -50,13 +50,24 @@ const newMessagesHandlerCreator = (dispatch: Dispatch) => {
   return _newMessagesHandler;
 };
 
+let _newStatusHandler: ((status: TStatus) => void) | null = null;
+let newStatusHandlerCreator = (dispatch: Dispatch) => {
+  if (_newStatusHandler === null) {
+    _newStatusHandler = (status) => {
+      dispatch(actions.statusChanged(status));
+    };
+  }
+};
+
 export const listenMessages = (): ThunkType => async (dispatch) => {
   chatAPI.start();
-  chatAPI.subscribe(newMessagesHandlerCreator(dispatch));
+  chatAPI.subscribe('messages-received', newMessagesHandlerCreator(dispatch));
+  chatAPI.subscribe('status-changed', newMessagesHandlerCreator(dispatch));
 };
 
 export const stopListenMessages = (): ThunkType => async (dispatch) => {
-  chatAPI.unsubscribe(newMessagesHandlerCreator(dispatch));
+  chatAPI.unsubscribe('messages-received', newMessagesHandlerCreator(dispatch));
+  chatAPI.unsubscribe('status-changed', newMessagesHandlerCreator(dispatch));
   chatAPI.stop();
 };
 
