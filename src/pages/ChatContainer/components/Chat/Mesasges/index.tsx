1c92413f96e3292af7a5eb8a2c0b0c780b773ba6
DavidCodeRussia <93557790+DavidCodeRussia@users.noreply.github.com>
2023-03-21 21:02:30 +0400

    do work with autoscroll

--- src/pages/ChatContainer/components/Chat/Mesasges/index.tsx
+++ src/pages/ChatContainer/components/Chat/Mesasges/index.tsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useRef, useState } from "react";
-import { useSelector } from "react-redux";
-import { AppStateType } from "../../../../../redux/redux-store";
-import Message from "../Mesasge";
+import React, { useEffect, useRef, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { AppStateType } from '../../../../../redux/redux-store';
+import Message from '../Mesasge';
 
 const Messages = () => {
   const messages = useSelector((state: AppStateType) => state.chat.messages);
@@ -9,17 +9,24 @@ const Messages = () => {
   const [isAutoScroll, setAutoScroll] = useState(true);
 
   const onScrollHandler = (e: React.UIEvent<HTMLDivElement, UIEvent>) => {
-    console.log("e", e);
+    const myDiv = e.currentTarget;
+    if (Math.abs(myDiv.offsetHeight + myDiv.scrollTop - myDiv.scrollHeight) < 100) {
+      setAutoScroll(true);
+    } else {
+      setAutoScroll(false);
+    }
   };
 
   useEffect(() => {
     if (isAutoScroll) {
-      newMessageRef.current?.scrollIntoView({ behavior: "smooth" });
+      setTimeout(() => {
+        newMessageRef.current?.scrollIntoView({ behavior: 'smooth' });
+      }, 500);
     }
   }, [messages]);
 
   return (
-    <div style={{ height: "400px", overflowY: "auto" }} onScroll={onScrollHandler}>
+    <div style={{ height: '400px', overflowY: 'auto' }} onScroll={onScrollHandler}>
       {messages !== undefined &&
         messages.map((item: any, index: number) => <Message key={index} message={item} />)}
       <div ref={newMessageRef}></div>
